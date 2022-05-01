95625361364fe9c71a7d4e260e6afbeb9ccacbea
SardinhaK <103686205+SardinhaK@users.noreply.github.com>
2022-04-30 21:06:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,49 +6,88 @@
 typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
 
 int main(){
+    // Carregamento das variaveis
     int eixoX = 900, eixoY=600;
-    Vector2 posPlayer = {50, 50};
+    Vector2 posPlayer = {50, 425};
     GameScreen currentScreen = LOGO;
-    int boxASpeedX = 4;
-    bool pause = false;             
-    bool collision = false;
     float telaPassando = 0.0f;
+    int frameAtual = 0;
+    int totalFrame = 7;
+    float time = 0.0f;
+    int direcao = 0;
 
+    //Abrir a tela
     InitWindow(eixoX, eixoY, "Ulala");
     SetTargetFPS(60);
 
+    //Carregamento das texturas
     Texture fundo = LoadTexture("assets/Mission 1.png");
     Texture fundoI = LoadTexture("assets/images.png");
     Texture fundoF = LoadTexture("assets/final.png");
+    Texture personagemJoinha = LoadTexture("assets/MarcoJoinha.png");
+    Texture personagemDireita = LoadTexture("assets/MarcoD.png");
+    Texture personagemEsquerda = LoadTexture("assets/MarcoE.png");
     currentScreen = TITLE;
-
+    
     while (!WindowShouldClose()){
-        if (IsKeyDown(KEY_RIGHT)) posPlayer.x += 4.0f;
-        if (IsKeyDown(KEY_LEFT)) posPlayer.x -= 4.0f;
-        if (IsKeyDown(KEY_UP)) posPlayer.y -= 4.0f;
-        if (IsKeyDown(KEY_DOWN)) posPlayer.y += 4.0f;
-        if (telaPassando <= -fundo.width) telaPassando = 0; 
-       
 
+        time += GetFrameTime();
+        if(time >= 0.1f){
+            time = 0.0f;
+            frameAtual += 1;
+        }
+        frameAtual = frameAtual % totalFrame;
 
         switch(currentScreen) // Tela de fundo
         {
             case TITLE:
             {
+                // Condição de ir a proxima tela
                 if (IsKeyPressed(KEY_ENTER)){
                     currentScreen = GAMEPLAY;
                 }
             } break;
             case GAMEPLAY:
             {
-                telaPassando -= 3.0f;
+                //Controles do personagem
+                if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)){
+                    posPlayer.x += 4.0f;
+                    direcao = 1;
+                    totalFrame = 11;
+                }else if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)){
+                    posPlayer.x -= 4.0f;
+                    direcao = 2;
+                    totalFrame = 11;
+                }else if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W) || IsKeyDown(KEY_SPACE)){ // Pulo nao implementado
+                    //posPlayer.y -= 4.0f;
+                    //direcao = 3;
+                }else if (IsKeyDown(KEY_J)){
+                    direcao = 5;
+                }else{
+                    direcao = 0;
+                    totalFrame = 7;
+                }
 
+                //Posição do player se limitando a meia tela e sem voltar cena
+                if(posPlayer.x >= 450){
+                telaPassando -= 4.0f;
+                posPlayer.x -= 4.0f;
+                }else if(posPlayer.x <= 0){
+                posPlayer.x += 4.0f;
+                }
+
+                
+                //Tela infinita para frente -->
+                if (telaPassando <= -fundo.width) telaPassando = 0; 
+               
+                //Condição para passar a proxima tela
                 if (IsKeyPressed(KEY_ENTER)){
                     currentScreen = ENDING;
                 }
             } break;
             case ENDING:
             {
+                //Condição para passar a proxima tela
                 if (IsKeyPressed(KEY_ENTER)){
                     currentScreen = TITLE;
                 }
@@ -64,21 +103,33 @@ int main(){
             {
                 case TITLE:
                 {
-                    DrawTexture(fundoI, 10, 10, WHITE);
+                    DrawTexture(fundoI, 0, 0, WHITE);
                     DrawText("Pau neles", 50, 20, 40, DARKGREEN);
-                    DrawText("Para começar aperte o enter", 50, 20, 20, DARKGREEN);
+                    DrawText("Para começar aperte o enter", 50, 50, 20, DARKGREEN);
 
                 } break;
                 case GAMEPLAY:
                 {
-                    DrawTextureEx(fundo, (Vector2){ telaPassando, 0 }, 0.0f, 1.0f, WHITE);
-                    DrawTextureEx(fundo, (Vector2){ fundo.width + telaPassando, 0 }, 0.0f, 1.0f, WHITE);
-                    DrawCircleV(posPlayer, 10, BLACK);
+                    if(direcao == 0){       //Caso ele fique parado
+                        DrawTextureEx(fundo, (Vector2){ telaPassando, 0 }, 0.0f, 1.0f, WHITE);
+                        DrawTextureEx(fundo, (Vector2){ fundo.width + telaPassando, 0 }, 0.0f, 1.0f, WHITE);
+                        DrawTextureRec(personagemJoinha,(Rectangle) {(personagemJoinha.width /totalFrame)*frameAtual, 0, personagemJoinha.width/totalFrame, personagemJoinha.height}, posPlayer, WHITE);
 
+                    }else if(direcao ==1) {     // Caso ande para direita
+                        DrawTextureEx(fundo, (Vector2){ telaPassando, 0 }, 0.0f, 1.0f, WHITE);
+                        DrawTextureEx(fundo, (Vector2){ fundo.width + telaPassando, 0 }, 0.0f, 1.0f, WHITE);
+                        DrawTextureRec(personagemDireita,(Rectangle) {(personagemDireita.width /totalFrame)*frameAtual, 0, personagemDireita.width/totalFrame, personagemDireita.height}, posPlayer, WHITE);
+
+                    }else if(direcao == 2){     //Caso ande a esquerda
+                        DrawTextureEx(fundo, (Vector2){ telaPassando, 0 }, 0.0f, 1.0f, WHITE);
+                        DrawTextureEx(fundo, (Vector2){ fundo.width + telaPassando, 0 }, 0.0f, 1.0f, WHITE);
+                        DrawTextureRec(personagemEsquerda,(Rectangle) {(personagemEsquerda.width /totalFrame)*frameAtual, 0, personagemEsquerda.width/totalFrame, personagemEsquerda.height}, posPlayer, WHITE);
+                    }
+                   
                 } break;
                 case ENDING:
                 {
-                    DrawTexture(fundoF, 10, 10, WHITE);
+                    DrawTexture(fundoF, 0, 0, WHITE);
                     DrawText("Morreu? NOOB!!!", 50, 20, 40, DARKBLUE);
                     DrawText("Para voltar a tela inicial aperte o enter", 50, 50, 20, DARKBLUE);
 
@@ -96,6 +147,9 @@ int main(){
     UnloadTexture(fundo);
     UnloadTexture(fundoI);
     UnloadTexture(fundoF);
+    UnloadTexture(personagemJoinha);
+    UnloadTexture(personagemDireita);
+    UnloadTexture(personagemEsquerda);
     CloseWindow();
     return 0;
 }
